aa142ce61af47b3cf44e84ef959dc347fea853f9
lynia-momo-202 <lyniamomo@gmail.com>
2025-01-13 13:28:13 +0100

    feat(projet): add  product_card_projet

--- 15_react_projects/src/index.js
+++ 15_react_projects/src/index.js
@@ -8,8 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-Module not found: Error: Can't resolve 'web-vitals' in '/workspaces/React_Projects/15_react_projects/src'
-ERROR in ./src/reportWebVitals.js 5:4-24
-Module not found: Error: Can't resolve 'web-vitals' in '/workspaces/React_Projects/15_react_projects/src'
-
-webpack compiled with 1 error
---
+++ 15_react_projects/src/projects/project1/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <h1 className='heading'>shop your way</h1>
+    </header>
+  )
+}
+
+export default Header
