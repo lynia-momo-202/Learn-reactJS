1dc4ff94f88590ab2341db1f135503cc533e7b1e
lynia-momo-202 <lyniamomo@gmail.com>
2025-01-16 13:52:42 +0100

    chore (src): add-product-card

--- 15_react_projects/src/projects/project2/Random.js
+++ 15_react_projects/src/projects/project2/Random.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import './App.css';
 function Random() {
   return (
-    <div>
-        <h1> Random Products</h1>
+    <div className='product-container'>
+      <h1> Product Title</h1>
+      <img src='' alt='' />
+      <p>You have seen 1 Product</p>
+      <button>Generate Random Product</button>
     </div>
   )
 }
