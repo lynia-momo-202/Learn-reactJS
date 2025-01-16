4d6ea0f4dfa7659894a65a064ee85a105c27fd3b
lynia-momo-202 <lyniamomo@gmail.com>
2025-01-16 14:27:14 +0100

    feat(project2): get a random product in fakestore api product list

--- 15_react_projects/src/projects/project2/Random.js
+++ 15_react_projects/src/projects/project2/Random.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import './App.css';
 function Random() {
+
+  async function getProduct(){
+    try {
+      //recuperer une liste de produit en utilisant l'api fakestore
+      const res = await fetch('https://fakestoreapi.com/products'); 
+      //rendre la liste de produits au format json
+      const data = await res.json();
+      console.log(data);
+      //generer un nombre entier inferieur au nombre d'element de la liste
+      const randomNumber = Math.floor(Math.random()*data.length);
+      console.log(randomNumber);
+      //recuperation du produit d'indice genere
+      const randomProduct = data[randomNumber-1];
+      console.log(randomProduct);
+    } catch (error) {
+      console.log('failed to fetch the product', error);
+    }
+  }
+
   return (
     <div className='product-container'>
       <h1> Product Title</h1>
       <img src='' alt='' />
       <p>You have seen 1 Product</p>
-      <button>Generate Random Product</button>
+      <button onClick={getProduct}>Generate Random Product</button>
     </div>
   )
 }
