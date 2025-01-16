f26a92ca376bd4762409a9867d6f0f8512beb5f0
lynia-momo-202 <lyniamomo@gmail.com>
2025-01-16 16:19:01 +0100

    chore(projet2): display randow product

--- 15_react_projects/src/projects/project2/Random.js
+++ 15_react_projects/src/projects/project2/Random.js
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import Message from './Message';
 function Random() {
+
+  const [product, setProduct] = useState(
+    {
+      title : '',
+      image : '',  
+    }
+  );
+  const [count, setCount] = useState(0)  ;
+  useEffect(()=>{
+    getProduct()
+  },[])
 
   async function getProduct(){
     try {
       //recuperer une liste de produit en utilisant l'api fakestore
       const res = await fetch('https://fakestoreapi.com/products'); 
+
       //rendre la liste de produits au format json
       const data = await res.json();
-      console.log(data);
+
       //generer un nombre entier inferieur au nombre d'element de la liste
       const randomNumber = Math.floor(Math.random()*data.length);
-      console.log(randomNumber);
+
       //recuperation du produit d'indice genere
       const randomProduct = data[randomNumber-1];
-      console.log(randomProduct);
+
+      //modification du state product
+      const newProduit = {title : randomProduct.title,image : randomProduct.image}
+      setProduct(newProduit);
     } catch (error) {
       console.log('failed to fetch the product', error);
     }
+    setCount(count+1);
+
   }
 
   return (
     <div className='product-container'>
-      <h1> Product Title</h1>
-      <img src='' alt='' />
-      <p>You have seen 1 Product</p>
+      <h1> {product.title? product.title : 'Product Title'}</h1>
+      <img src={product.image} alt={product.title} />
+      <Message count = {count}/>
       <button onClick={getProduct}>Generate Random Product</button>
     </div>
   )
