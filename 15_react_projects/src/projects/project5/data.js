f2ece8c70da6235283894f314af81b7237be2c42
lynia-momo-202 <lyniamomo@gmail.com>
2025-01-24 13:09:27 +0100

    chore(project5)initial

---
+++ 15_react_projects/src/projects/project5/data.js
@@ -0,0 +1,26 @@
+export const  data=[
+    {
+        id:1,
+        name:'Khaiser Khanam',
+        age:25,
+        image:"../assets/friend1.png"
+    },
+    {
+        id:2,
+        name:'Nehal Kumar',
+        age:28,
+        image:"../assets/friend2.png"
+    },
+    {
+        id:3,
+        name:'Neha Sharma',
+        age:26,
+        image:"../assets/friend3.png"
+    },
+    {
+        id:4,
+        name:'Mehul Mehta',
+        age:26,
+        image:"../assets/friend4.png"
+    }
+]
