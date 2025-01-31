8397f0fb1334e884f486948aee4378b239504c7f
lynia-momo-202 <lyniamomo@gmail.com>
2025-01-31 14:40:43 +0100

    chrore(project4):add accordion item

--- 15_react_projects/src/projects/project4/Accordion.js
+++ 15_react_projects/src/projects/project4/Accordion.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import './App.css'
+import AccordionItem from './AccordionItem'
+import AccordionSingleItem from './AccordionSingleItem'
+import data from './data'
 
 function Accordion() {
-  return (
-    <div>Accordion</div>
-  )
+    let i = 1 ;
+    return (
+        <main className='accordion-container'>
+            <p className='faq'>
+                <strong>
+                    Frequently Asked Questions (FAQ's)
+                </strong>
+            </p>
+
+            {
+                data.map((faq) => (
+                    <AccordionItem 
+                    key={i}
+                    number = {i++}
+                    question = { faq.question}
+                    answer = {faq.answer}
+                    />
+                    
+                )
+            )
+            }
+            <p className='faq'>
+                ------------------------
+            </p>
+            <AccordionSingleItem />
+        </main>
+    )
 }
 
 export default Accordion
--- 15_react_projects/src/projects/project4/AccordionItem.js
+++ 15_react_projects/src/projects/project4/AccordionItem.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function AccordionItem() {
-  return (
-    <div>AccordionItem</div>
-  )
+function AccordionItem(props) {
+    const {number,question,answer} = props
+    const [show,setShow] = useState(0)
+    const handleshow = () =>{
+        // alert("hllo")
+        {show? setShow(0) : setShow(1)}
+    }
+    return (
+        <div className={`accordion ${show? 'active' : ''}`}>
+            <div className='accordion-header ' onClick={handleshow}>
+                <span className='accordion-icon'>
+                    {show? '-' : '+'}
+                    </span>
+                <span className='accordion-number'>{number}</span>
+                <span className='accordion-title'>{question}</span>
+            </div>
+            <div className='accordion-content'>
+            {show? answer : ''}
+            </div>
+        </div>
+    )
 }
 
 export default AccordionItem
