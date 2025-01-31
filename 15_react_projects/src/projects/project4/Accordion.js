2ab27116ab7a10522c5f4a39e3de31983c11b7ef
lynia-momo-202 <lyniamomo@gmail.com>
2025-01-31 15:57:49 +0100

    chore(project4): add accordion single Item

--- 15_react_projects/src/projects/project4/Accordion.js
+++ 15_react_projects/src/projects/project4/Accordion.js
@@ -1,35 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import AccordionItem from './AccordionItem'
 import AccordionSingleItem from './AccordionSingleItem'
 import data from './data'
 
 function Accordion() {
-    let i = 1 ;
+    let i = 1, j = 1;
+    const [select, setSelect] = useState(null)
+
+    const handleshowItem = (id) => {
+        // alert("hllo")
+        setSelect(id !== select ? id : null);
+    }
+
     return (
         <main className='accordion-container'>
+
             <p className='faq'>
                 <strong>
                     Frequently Asked Questions (FAQ's)
                 </strong>
             </p>
-
             {
                 data.map((faq) => (
-                    <AccordionItem 
-                    key={i}
-                    number = {i++}
-                    question = { faq.question}
-                    answer = {faq.answer}
+                    <AccordionItem
+                        key={i}
+                        number={i++}
+                        question={faq.question}
+                        answer={faq.answer}
                     />
-                    
+
                 )
-            )
+                )
             }
             <p className='faq'>
                 ------------------------
             </p>
-            <AccordionSingleItem />
+
+            {
+                data.map((faq) => (
+                    <AccordionSingleItem
+                        key={j}
+                        number={j++}
+                        question={faq.question}
+                        answer={faq.answer}
+                        select = {select}
+                        handleshowItem={handleshowItem}
+                    />
+
+                )
+                )
+            }
+
         </main>
     )
 }
--- 15_react_projects/src/projects/project4/AccordionSingleItem.js
+++ 15_react_projects/src/projects/project4/AccordionSingleItem.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
-function AccordionSingleItem() {
-  return (
-    <div>AccordionSingleItem</div>
-  )
+function AccordionSingleItem(props) {
+    const { number, question, answer, select ,handleshowItem} = props
+    
+    return (
+        <div className={`accordion ${number === select ? 'active' : ''}`}>
+            <div className='accordion-header ' onClick={() => handleshowItem(number)}>
+                <span className='accordion-icon'>
+                    {number === select ? '-' : '+'}
+                </span>
+                <span className='accordion-number'>{number}</span>
+                <span className='accordion-title'>{question}</span>
+            </div>
+            <div className='accordion-content'>
+                {number === select ? answer : ''}
+            </div>
+        </div>
+    )
 }
 
 export default AccordionSingleItem
